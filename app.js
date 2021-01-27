55f51234efe76a546f9796715fa9cfcd34eb4c0f
Rishabh <rjexpert.coding@gmail.com>
2021-01-27 13:36:02 +0530

    ejs template engine

--- app.js
+++ app.js
@@ -8,17 +8,20 @@ const app = express(); //creating a server
 const cors = require('cors')
 app.use(cors());  //cross origin request
 
+app.set("views","./views");
 app.use(express.static("public"));
+app.set("view engine","ejs");
 
 const http = require("http").createServer(app); //creating a port via app same used by socket.io
 
 //pick code from public folder w ebought from frontend
 
 app.get("/", function (request, response) {
-    response.redirect("/index.ejs");
+    // response.redirect("index.ejs");
+    response.render('index', { title: 'Hey', message: 'Hello there!' });
 })
 
-let port = 3000;
+let port = process.env.PORT || 3000;
 http.listen(port, () => {
     console.log('server is listening at 3000 port');
 });
